51e1e50046ec2a23b32335305b2f1c772f8605d6
KarolMrozInfoShareStudent <polityk357@gmail.com>
2018-10-13 15:37:51 +0200

    dodanie ternary operatora

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,52 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
 
 const tasks = [
   {
     id: 1,
-    title: 'Janek Kowalski',
+    title: "Janek Kowalski",
     isDone: false,
     isImportant: false
   },
   {
     id: 2,
-    title: 'Rysiek z Klanu',
+    title: "Rysiek z Klanu",
     isDone: true,
     isImportant: true
   },
   {
     id: 3,
-    title: 'MAriusz Pudzianowki ',
+    title: "MAriusz Pudzianowki ",
     isDone: false,
     isImportant: true
   }
-]
+];
 
 class App extends Component {
-
-
-
-
   render() {
     return (
       <div className="App">
-       <h1>Lista Kontakrów</h1>
-       <ul>
-          {
-            tasks.map(
-              task => (
-                <li key={task.id}>
-                <del>{task.title}</del>
-                </li>
-              )
-            )
-          }
+        <h1>Lista Kontakrów</h1>
+        <ul>
+          {tasks.map(task => (
+            <li key={task.id}>
+            {
+              task.isDone ? <del>{task.title}</del> : task.title
+            }
+            </li>
+          ))}
         </ul>
-     
       </div>
-      
-      
     );
   }
 }
