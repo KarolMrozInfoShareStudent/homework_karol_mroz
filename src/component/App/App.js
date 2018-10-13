6ce24f88703495b91df3d0dcd96edab1d054b3fe
KarolMrozInfoShareStudent <polityk357@gmail.com>
2018-10-13 15:28:48 +0200

    sepeate directoryfor components and Iterate contacts name

---
+++ src/component/App/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+const tasks = [
+  {
+    id: 1,
+    title: 'Janek Kowalski',
+    isDone: false,
+    isImportant: false
+  },
+  {
+    id: 2,
+    title: 'Rysiek z Klanu',
+    isDone: true,
+    isImportant: true
+  },
+  {
+    id: 3,
+    title: 'MAriusz Pudzianowki ',
+    isDone: false,
+    isImportant: true
+  }
+]
+
+class App extends Component {
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+       <h1>Lista Kontakrów</h1>
+       <ul>
+          {
+            tasks.map(
+              task => (
+                <li>{task.title}</li>
+              )
+            )
+          }
+        </ul>
+     
+      </div>
+      
+      
+    );
+  }
+}
+
+export default App;
