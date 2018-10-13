4d753bb1f16874d5b9d5132ed6729a5e446ecb4a
KarolMrozInfoShareStudent <polityk357@gmail.com>
2018-10-13 16:03:02 +0200

    dodanie tablicy

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,38 +1,39 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const tasks = [
-  {
-    id: 1,
-    title: "Janek Kowalski",
-    isDone: false,
-    isImportant: false
-  },
-  {
-    id: 2,
-    title: "Rysiek z Klanu",
-    isDone: true,
-    isImportant: true
-  },
-  {
-    id: 3,
-    title: "MAriusz Pudzianowki ",
-    isDone: false,
-    isImportant: true
-  }
-];
-
 class App extends Component {
+  state = {
+    tasks: [
+      {
+        id: 1,
+        title: "Janek Kowalski",
+        isDone: false,
+        isImportant: false
+      },
+      {
+        id: 2,
+        title: "Rysiek z Klanu",
+        isDone: true,
+        isImportant: true
+      },
+      {
+        id: 3,
+        title: "Marek z Warszwy",
+        isDone: false,
+        isImportant: true
+      }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>Lista Kontakrów</h1>
         <ul>
-          {tasks.map(task => (
+          {
+            this.state.tasks.map(
+              task => (
             <li key={task.id}>
-              {
-                task.isImportant && <span>&#9734;</span>
-              }
+              {task.isImportant ? <span>&#9733;</span> : <span>&#9734;</span>}
 
               {task.isDone ? <del>{task.title}</del> : task.title}
             </li>
